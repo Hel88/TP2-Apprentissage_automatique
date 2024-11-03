a55365ad7b8c99a2e0a978d8ac23a30a6bee378a
Hel88 <helena.barbillon@orange.fr>
2024-11-03 17:41:14 -0500

    Début implémentation UMAP

--- src/main.py
+++ src/main.py
@@ -12,3 +12,26 @@ print(covertype.metadata)
 
 # variable information
 print(covertype.variables)
+
+
+import umap
+from umap.parametric_umap import ParametricUMAP
+from sklearn.preprocessing import StandardScaler
+
+#
+# # Pré-traitements
+# from sklearn.impute import SimpleImputer
+# imputer = SimpleImputer(strategy="mean")  # Choisir la stratégie (mean, median, most_frequent)
+# X = imputer.fit_transform(X)
+#
+# #standardiser données
+# scaler = StandardScaler()
+# X_scaled = scaler.fit_transform(X)
+
+
+# reducer = umap.UMAP(n_neighbors=15, min_dist=0.1)
+# embedding = reducer.fit(X_scaled)
+
+# ### version paramétrique
+# embedder = ParametricUMAP()
+# embedding = embedder.fit_transform(X)
