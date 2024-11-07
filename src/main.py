ae5660fe8915248afffb0c9327718e4416122668
Hel88 <helena.barbillon@orange.fr>
2024-11-07 16:09:09 -0500

    Visualisations algo umap

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 from ucimlrepo import fetch_ucirepo
+import umap
+from umap.parametric_umap import ParametricUMAP
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
 
 # fetch dataset
+print("fetch data")
 covertype = fetch_ucirepo(id=31)
+print("done fetching data")
+
 
 # data (as pandas dataframes)
 X = covertype.data.features
@@ -13,25 +21,29 @@ print(covertype.metadata)
 # variable information
 print(covertype.variables)
 
+# Sélectionner seulement 10% de la base de données au hasard
+X_sample, _, y_sample, _ = train_test_split(X, y, train_size=0.1, stratify=y, random_state=42)
+print(X_sample.shape)
+print(y_sample.shape)
 
-import umap
-from umap.parametric_umap import ParametricUMAP
-from sklearn.preprocessing import StandardScaler
 
 #
-# # Pré-traitements
-# from sklearn.impute import SimpleImputer
-# imputer = SimpleImputer(strategy="mean")  # Choisir la stratégie (mean, median, most_frequent)
-# X = imputer.fit_transform(X)
+# # # Pré-traitements
+# # from sklearn.impute import SimpleImputer
+# # imputer = SimpleImputer(strategy="mean")  # Choisir la stratégie (mean, median, most_frequent)
+# # X = imputer.fit_transform(X)
 #
 # #standardiser données
 # scaler = StandardScaler()
 # X_scaled = scaler.fit_transform(X)
-
-
-# reducer = umap.UMAP(n_neighbors=15, min_dist=0.1)
-# embedding = reducer.fit(X_scaled)
-
-# ### version paramétrique
-# embedder = ParametricUMAP()
-# embedding = embedder.fit_transform(X)
+#
+# print("Lancement de UMAP...")
+# reducer = umap.UMAP()
+# embedding = reducer.fit_transform(X_scaled)
+# print("Umap fini")
+# print(embedding.shape)
+#
+#
+# # ### version paramétrique
+# # embedder = ParametricUMAP()
+# # embedding = embedder.fit_transform(X)
