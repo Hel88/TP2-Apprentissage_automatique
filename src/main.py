1b277952874cc1de67ee23de286c963aa106606b
Hel88 <helena.barbillon@orange.fr>
2024-11-19 11:47:28 -0500

    Oversampling avec SMOTE

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from ucimlrepo import fetch_ucirepo
 from UMAP_module import UMAP_module
+from imblearn.over_sampling import SMOTE
+
 
 
 # Récupérer le dataset
@@ -15,19 +17,34 @@ y = covertype.data.targets
 
 print(X.shape)
 print(y.shape)
-
 print(y.Cover_Type.value_counts())
 
 
 # Réduire le dataset
-new_size = 0.2   # pourcentage du dataset qu'on veut garder
+new_size = 0.5   # pourcentage du dataset qu'on veut garder
 X_sampled, _, y_sampled, _ = train_test_split(X, y, train_size=new_size, stratify=y, random_state=42)
+
 
 # Standardiser données
 scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X_sampled)
+X_scaled = scaler.fit_transform(X)
+
+print ("before over-sampling")
+print(y.Cover_Type.value_counts())
+
+
+# Over-sampling avec SMOTE
+sm = SMOTE(random_state=42, k_neighbors=3)
+print("Running SMOTE over-sampling...")
+X_oversampled, y_oversampled = sm.fit_resample(X_scaled, y_sampled)
+print("SMOTE done\n")
+
+
+print("After over-sampling: ")
+print(y_oversampled.Cover_Type.value_counts())
+
 
 # UMAP visualisation
-umap = UMAP_module(X_scaled, y_sampled)
+umap = UMAP_module(X_oversampled, y_oversampled)
 umap.run_algo()
-umap.display("UMAP plotting with 50% of the dataset", "umap_20%_of_dataset")
+umap.display("UMAP plotting with all of the dataset oversampled with SMOTE", "umap_SMOTE")
